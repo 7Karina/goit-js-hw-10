eafba8cba51a6bb00cf9a9f822abfb7acbfb37f4
@7Karina <karinafrank750@gmail.com>
2023-10-26 16:34:02 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-fetch();
+fetch('https://api.thecatapi.com/v1/breeds')
+  .then(resp => {
+    if (!resp.ok) {
+      throw new Error();
+    }
+    return resp.json();
+  })
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
