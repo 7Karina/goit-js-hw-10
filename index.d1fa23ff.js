77d4795145e5715fbd7c353e26942f81d1108fbc
7Karina <133170382+7Karina@users.noreply.github.com>
2023-10-29 21:36:00 +0000

    Deploying to gh-pages from @ 7Karina/goit-js-hw-10@c1c85c5ff4c510d4070e2bd3546971e9d1b19c12 🚀

--- index.d1fa23ff.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds").then((o=>{if(!o.ok)throw new Error;return o.json()})).then((o=>console.log(o))).catch((o=>console.log(o)));
-//# sourceMappingURL=index.d1fa23ff.js.map
